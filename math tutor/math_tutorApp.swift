a562541cf4e29316520363bbf05fc9ba7ce7505e
Evank221 <evanharo49@gmail.com>
2026-01-27 10:07:32 -0800

    Initial Commit

---
+++ math tutor/math_tutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_tutorApp.swift
+//  math tutor
+//
+//  Created by HARO, EVAN on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct math_tutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ math tutorTests/math_tutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  math_tutorTests.swift
+//  math tutorTests
+//
+//  Created by HARO, EVAN on 1/27/26.
+//
+
+import Testing
+@testable import math_tutor
+
+struct math_tutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
